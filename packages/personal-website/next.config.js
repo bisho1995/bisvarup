43db96b794f3f68de4b3d642ec46b1d0ff2497a7
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-05-16 13:39:37 +0530

    removed custom build folder

--- packages/personal-website/next.config.js
+++ packages/personal-website/next.config.js
@@ -1,11 +1,9 @@
 const withSass = require("@zeit/next-sass");
 
 module.exports = (phase, { defaultConfig }) => {
-  console.log({ phase, defaultConfig });
   return withSass({
     compress: true,
     poweredByHeader: false,
-    distDir: "build",
     env: {
       customKey: "my-value",
     },
